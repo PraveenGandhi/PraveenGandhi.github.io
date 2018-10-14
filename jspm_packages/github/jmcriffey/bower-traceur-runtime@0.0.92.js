3233b312df392b0e78eb106f5402fafdea16d85c
JaiRam <praveengandhi.p@gmail.com>
2018-10-14 19:16:10 +0530

    renshuu app 練習アプリ

--- jspm_packages/github/jmcriffey/bower-traceur-runtime@0.0.92.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("github:jmcriffey/bower-traceur-runtime@0.0.92/traceur-runtime");
--- jspm_packages/npm/aurelia-materialize-bridge@0.5.1.js
+++
@@ -1,3 +0,0 @@
-define(["npm:aurelia-materialize-bridge@0.5.1/index"], function(main) {
-  return main;
-});
