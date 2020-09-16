66a32d1b29178bece73d309452c8c033444758bd
PraveenGandhi <praveengandhi.p@gmail.com>
2020-09-16 22:03:03 +0530

    app begin and finish

--- a/index.js
+++ a/index.js
@@ -1,14 +1,23 @@
 (function () {
-
     const storageKey = 'items';
-    const table = document.querySelector('tbody');
-    let items = localStorage.getItem(storageKey) ? JSON.parse(localStorage.getItem(storageKey)) : [];
+    const stateKey = 'state';
 
+    const tbody = document.querySelector('tbody');
+    const tableElement = document.querySelector('.table');
+    const beginButton = document.querySelector('.begin');
+    const finishButton = document.querySelector('.finish');
+    const messageElement = document.querySelector('.message');
+
+    let items = localStorage.getItem(storageKey) ? JSON.parse(localStorage.getItem(storageKey)) : [];
+    let appState = localStorage.getItem(stateKey) ? localStorage.getItem(storageKey) : '';
+    
     const saveToLocalStorage = data => localStorage.setItem(storageKey, JSON.stringify(data));
 
-    const toggleDone = text => {
-        let item = items.find(i => i.name === text.replace(/\d+\.\s+/, ''));
+    const toggleDone = e => {
+        let item = items.find(i => i.name === e.target.textContent.replace(/\d+\.\s+/, ''));
         item.done = !item.done;
+        e.currentTarget.classList.remove('table-danger');
+        e.currentTarget.classList.toggle('table-success');
         saveToLocalStorage(items);
     };
 
@@ -19,18 +28,57 @@
         td1.textContent = `${index + 1}.`;
         td.textContent = item.name;
         if (item.done) tr.classList.add('table-success');
-        tr.addEventListener('click', e => {
-            toggleDone(e.target.textContent);
-            e.currentTarget.classList.toggle('table-success');
-        });
+        tr.addEventListener('click', toggleDone);
         tr.appendChild(td1);
         tr.appendChild(td);
-        table.appendChild(tr);
+        tbody.appendChild(tr);
     };
 
     const getChecklistItems = data => data.split('\n').filter(i => i);
 
     const toObject = name => { return { name }; };
+
+    const removeAllChildNodes = parent => {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    const begin = () => {
+        localStorage.setItem(stateKey, 'stared');
+        saveToLocalStorage([]);
+        startApp();
+    };
+
+    const finish = () => {
+        let unChecked = items.filter(i => !i.done);
+        if(unChecked.length) {
+            Array.from(document.querySelectorAll('tr:not(.table-success)')).forEach(node=> node.classList.add('table-danger'))
+            return;
+        }
+        localStorage.setItem(stateKey, '');
+        saveToLocalStorage([]);
+        removeAllChildNodes(tbody);
+        messageElement.classList.remove('d-none');
+        tableElement.classList.add('d-none');
+        document.querySelector('.finish.btn').classList.add('d-none');
+        items = [];
+    };
+
+    const startApp = () => {
+        if(!messageElement.classList.contains('d-none')){
+            messageElement.classList.add('d-none');
+        }
+        tableElement.classList.remove('d-none');
+        document.querySelector('.finish.btn').classList.remove('d-none');
+        fetch(`data.txt?t=${Math.floor(Date.now())}`)
+            .then(response => response.text())
+            .then(dataCallback);
+
+        finishButton.addEventListener('click', finish);
+
+        window.items = items;
+    };
 
     const compareData = (data) => {
         let tempData = getChecklistItems(data);
@@ -54,49 +102,11 @@
         items.forEach(makeElement);
     };
 
-    fetch(`data.txt?t=${Math.floor(Date.now())}`)
-        .then(response => response.text())
-        .then(dataCallback);
+    if (appState !== 'stared') {
+        messageElement.classList.remove('d-none');
+        beginButton.addEventListener('click', begin);
+        return;
+    }
 
-    window.items = items;
-
+    startApp();
 })();
-
-// Register service worker to control making site work offline
-
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('sw.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
-  
-  // Code to handle install prompt on desktop
-  
-  let deferredPrompt;
-  const addBtn = document.querySelector('.add-button');
-  //addBtn.style.display = 'none';
-  
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
-  
-    addBtn.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      addBtn.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          deferredPrompt = null;
-        });
-    });
-  });
